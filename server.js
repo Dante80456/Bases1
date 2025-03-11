8fd6eadce413e2fadae26316b804a039de3a8fa9
unknown <daniel80456omg@gmail.com>
2025-03-10 20:34:52 -0500

    Corrigiendo backend para Vercel

--- server.js
+++ server.js
@@ -19,6 +19,7 @@ app.use(cors());
 app.use(bodyParser.json());
 app.use(express.static('public'));
 app.get('/favicon.ico', (req, res) => res.status(204).end());
+app.use(express.static(path.join(__dirname, 'public')));
 
 
 // Crear un nuevo libro

69f74908158a736c492032d6ea06f4bfc7cdd4a1
unknown <daniel80456omg@gmail.com>
2025-03-10 20:52:52 -0500

    Corrigiendo backend para Vercel

--- index.js
+++ index.js
@@ -1,3 +1,14 @@
-const app = require('./server');
+const express = require('express');
+const app = express();
+const path = require('path'); // <--- Agregar esto si falta
 
-export default app;
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, 'public', 'index.html'));
+});
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+    console.log(`Servidor corriendo en el puerto ${PORT}`);
+});

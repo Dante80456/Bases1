0772cebfb2bcac89fbaf9a4089f3e8850e620864
unknown <daniel80456omg@gmail.com>
2025-03-10 19:46:19 -0500

    Corrección del puerto y ruta API

--- api/server.js
+++ api/server.js
@@ -81,7 +81,4 @@ app.get('/api', (req, res) => {
     res.json({ message: 'API funcionando correctamente' });
 });
 
-const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => {
-    console.log(`Servidor corriendo en puerto ${PORT}`);
-});
+module.exports = app;
